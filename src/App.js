9042f59e47130c6a6b355b80c1028cc934a546ae
UtkarshSarkari <utkrsh2001@gmail.com>
2022-07-26 01:21:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Editor from './Pages/Editor'
+import Home from './Pages/Home'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Toaster
+    position='top-right'
+    toastOptions={{
+      success: {
+        theme: {
+          primary: '#4aed88'
+        },
+      },
+    }}
+    ></Toaster>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/editor/:roomId' element={<Editor />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
-export default App;
